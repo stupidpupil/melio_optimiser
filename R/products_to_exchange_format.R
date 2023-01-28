d6dc87bd2863e965060764fba60f9fedd4ac3111
Adam Watkins <adam@stupidpupil.co.uk>
2023-01-28 12:26:18 +0000

    Add exchange JSON output

---
+++ R/products_to_exchange_format.R
@@ -0,0 +1,32 @@
+products_to_exchange_format <- function(products){
+
+  biomarkers_map <- readr::read_csv("data-raw/biomarker_snomed_map.csv", col_types="cc")
+
+  products <- products |>
+    purrr::map(function(prod){
+      prod$id = NULL
+
+      prod$name = jsonlite::unbox(prod$title)
+      prod$title = NULL
+
+      prod$url = jsonlite::unbox(paste0("https://www.meliohealth.co.uk/product/", prod$handle))
+      prod$handle = NULL
+
+      prod$price_pence = jsonlite::unbox(prod$price_pence)
+      prod$turnaround_days = jsonlite::unbox(prod$turnaround_days)
+
+      prod$biomarkers <- tibble::tibble(biomarker_handle = prod$biomarkers) |>
+        dplyr::left_join(biomarkers_map,  by="biomarker_handle") |>
+        dplyr::pull("sctid") |>
+        na.omit() |>
+        unique()
+
+      return(prod)
+    }) |>
+    purrr::keep(function(prod){
+      length(prod$biomarkers) > 0
+    })
+
+
+  return(products)
+}
